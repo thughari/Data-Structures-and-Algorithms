cfa3d20f6ca44c39a3ce574518ad329feee99216
Vinith11 <vinithkommugalla11@gmail.com>
2024-06-12 11:27:14 +0530

    Selection Sort

--- src/Sorting/SelectionSort.java
+++ src/Sorting/SelectionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] nums ={6,5,8,9,4,1,10,34,0};
+        int[] nums ={6,5,8,9,4,1,1,10,34,0,0};
         System.out.println("Before Sorting: "+ Arrays.toString(nums));
         selectionSort(nums);
         System.out.println("After Sorting: "+Arrays.toString(nums));
@@ -12,5 +12,17 @@ public class SelectionSort {
 
     public static void selectionSort(int[] nums) {
         int temp=0;
+        int minIndex=0;
+        for (int i=0;i<nums.length;i++){
+            minIndex=i;
+            for(int j=i;j<nums.length;j++){
+                if (nums[j]<nums[minIndex]){
+                    minIndex=j;
+                }
+            }
+            temp=nums[i];
+            nums[i]=nums[minIndex];
+            nums[minIndex]=temp;
+        }
     }
 }
