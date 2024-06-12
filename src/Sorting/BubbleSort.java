a7ef3cd10777010582365d5c07f98dcea2f6f358
thughari <haribabutatikonda3@gmail.com>
2024-06-12 11:45:41 +0530

    thughari

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -11,7 +11,7 @@ public class BubbleSort { //Time complexity O(n^2)
     }
     public static void bubbleSort(int[] nums) {
         int temp=0;
-        for (int i=0;i<nums.length;i++) {
+        for (int i=0;i<nums.length;i++){
             for (int j=0;j<nums.length-1-i;j++) {
                 if (nums[j]>nums[j+1]){
                     temp=nums[j];
